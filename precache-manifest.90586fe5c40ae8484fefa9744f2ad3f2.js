f29142880c99bd3546bf5b30ba5b62677d2a793b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-18 04:02:54 +0000

    Deploy satyajitrpatnaik/profile to github.com/satyajitrpatnaik/profile.git:gh-pages

---
+++ precache-manifest.90586fe5c40ae8484fefa9744f2ad3f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1f21c5d614708217840db9500f3daf6",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "a8c3e8627df6b9026c44",
+    "url": "/profile/static/css/main.b3c64fa8.chunk.css"
+  },
+  {
+    "revision": "8ae6e1ea84f7acc7b5ff",
+    "url": "/profile/static/js/2.84284d5c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/profile/static/js/2.84284d5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8c3e8627df6b9026c44",
+    "url": "/profile/static/js/main.5e2349eb.chunk.js"
+  },
+  {
+    "revision": "4c6beef5c28ef76d0d59",
+    "url": "/profile/static/js/runtime-main.8cc17419.js"
+  }
+]);
